a797e1a9168821f50a9b9918f9146e99084c530c
Andrei Bota <andreibota00@gmail.com>
2021-11-18 22:05:16 +0200

    Created User Creation

--- pages/views.py
+++ pages/views.py
@@ -1,16 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse, request
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from users.forms import CreateUserForm
+from django.contrib import messages
+
 
 # Create your views here.
 
 
 def login_view(request, *args, **kwargs):
-    return render(request, "login.html", {})
+    context = {}
+    return render(request, "login.html", context)
 
 
 def signup_view(request, *args, **kwargs):
-    return render(request, "signup.html", {})
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Your Account Has Been Created! ')
+
+    context = {'form': form}
+    return render(request, "signup.html", context)
 
 
 def home_view(request, *args, **kwargs):
-    return render(request, "home.html", {})
+    context = {}
+    return render(request, "home.html", context)
---
+++ static/scripts/signup.js
@@ -0,0 +1,18 @@
+
+let form_username = document.getElementById('id_username')
+form_username.placeholder='Username';
+form_username.className += ' form-control';
+
+let form_email = document.getElementById('id_email')
+form_email.placeholder='Email';
+form_email.className += ' form-control';
+
+let form_password1 = document.getElementById('id_password1')
+form_password1.placeholder='Password';
+form_password1.className += ' form-control';
+
+let form_password2 = document.getElementById('id_password2')
+form_password2.placeholder='Confirm Password';
+form_password2.className += ' form-control';
+
+
