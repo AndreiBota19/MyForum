ee2fef7eaf763ceea0921743e5ed897eab9b1175
Andrei Bota <andreibota00@gmail.com>
2021-11-18 14:52:08 +0200

    Created app structure

--- MyForum/urls.py
+++ MyForum/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pages.views import home_view, signup_view, login_view
+
 urlpatterns = [
+
+    path('', home_view, name='home'),
+    path('login/', login_view, name='login'),
+    path('signup/', signup_view,name='signup'),
     path('admin/', admin.site.urls),
 ]
---
+++ pages/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse, request
+
+# Create your views here.
+
+def login_view(request, *args, **kwargs):
+    return render(request, "login.html", {})
+
+    
+def signup_view(request, *args, **kwargs):
+    return render(request, "signup.html", {})
+
+
+def home_view(request, *args, **kwargs):
+    return render(request, "home.html", {})
